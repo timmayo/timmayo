7439b7cbdc4148c23f78ebc02d34dae48f408721
Tim Mayo <42679227+timmayo@users.noreply.github.com>
2024-02-29 11:37:08 -0500

    Update UploadContentToSPO.ps1

--- UploadContentToSPO.ps1
+++ UploadContentToSPO.ps1
@@ -3,7 +3,7 @@ $cred = (Get-Credential admin@YOUR_DOMAIN.onmicrosoft.com)
 $sourceFiles = '\\FILE_SHARE\migration'
 $sourcePackage = 'c:\migration\source'
 $targetPackage = 'c:\migration\target'
-$targetWeb = 'https://mngenvmcap648104.sharepoint.com/sites/Migration'
+$targetWeb = 'https://YOUR_DOMAIN.sharepoint.com/sites/Migration'
 $targetDocLib = 'Test'
 
 New-SPOMigrationPackage -SourceFilesPath $sourceFiles -OutputPackagePath $sourcePackage -TargetWebUrl $targetWeb -TargetDocumentLibraryPath $targetDocLib -NoAzureADLookup
